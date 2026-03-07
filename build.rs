bba6cbcc6816e992dd8bf326d9186a30860e53db
DarkXero-dev <steve@techxero.com>
2026-03-07 19:33:42 +0200

    Fix

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 use cxx_qt_build::{CxxQtBuilder, QmlModule};
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/");
+    println!("cargo:rerun-if-changed=qml/");
+    println!("cargo:rerun-if-changed=build.rs");
     CxxQtBuilder::new_qml_module(QmlModule::new("com.howdy.gui").qml_files(["qml/main.qml"]))
         .file("src/bridge.rs")
         .build();
